fa23a6cb1ae0606fa4cbb98c3d999933db766e8b
Jason Tseng <tseng.frank@gmail.com>
2011-11-12 17:45:07 +0800

    support LZMA compression kernel

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 73 */
+/* This file is auto generated, version 74 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#73 PREEMPT Sat Nov 12 16:32:07 CST 2011"
-#define LINUX_COMPILE_TIME "16:32:07"
+#define UTS_VERSION "#74 PREEMPT Sat Nov 12 17:30:34 CST 2011"
+#define LINUX_COMPILE_TIME "17:30:34"
 #define LINUX_COMPILE_BY "jason"
 #define LINUX_COMPILE_HOST "linux-jjzq"
 #define LINUX_COMPILE_DOMAIN "site"
