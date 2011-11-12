5b9996e4802c6e1d3befeae8ee70ea0d04b01fcf
Jason Tseng <tseng.frank@gmail.com>
2011-11-12 16:40:55 +0800

    tweak smartass2

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 71 */
+/* This file is auto generated, version 73 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#71 PREEMPT Fri Nov 11 16:18:37 CST 2011"
-#define LINUX_COMPILE_TIME "16:18:37"
+#define UTS_VERSION "#73 PREEMPT Sat Nov 12 16:32:07 CST 2011"
+#define LINUX_COMPILE_TIME "16:32:07"
 #define LINUX_COMPILE_BY "jason"
 #define LINUX_COMPILE_HOST "linux-jjzq"
 #define LINUX_COMPILE_DOMAIN "site"
