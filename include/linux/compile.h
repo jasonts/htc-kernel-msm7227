2d7b330157a5bef481bddfe14a074c35514fc224
Jason Tseng <tseng.frank@gmail.com>
2012-02-23 16:50:53 +0800

    lib/crc: add slice by 8 algorithm to crc32.c Add support for slice by 8 to existing crc32 algorithm.  Also modify gen_crc32table.c to only produce table entries that are actually used. The parameters CRC_LE_BITS and CRC_BE_BITS determine the number of bits in the input array that are processed during each step.  Generally the more bits the faster the algorithm is but the more table data required.
    Using an x86_64 Opteron machine running at 2100MHz the following table was
    collected with a pre-warmed cache by computing the crc 1000 times on a
    buffer of 4096 bytes.
    
    BITS Size LE Cycles/byte BE Cycles/byte
    ----------------------------------------------
    1 873 41.65 34.60
    2 1097 25.43 29.61
    4 1057 13.29 15.28
    8 2913 7.13 8.19
    32 9684 2.80 2.82
    64 18178 1.53 1.53
    
    BITS is the value of CRC_LE_BITS or CRC_BE_BITS. The old
    default was 8 which actually selected the 32 bit algorithm. In
    this version the value 8 is used to select the standard
    8 bit algorithm and two new values: 32 and 64 are introduced
    to select the slice by 4 and slice by 8 algorithms respectively.
    
    Where Size is the size of crc32.o's text segment which includes
    code and table data when both LE and BE versions are set to BITS.
    
    The current version of crc32.c by default uses the slice by 4 algorithm
    which requires about 2.8 cycles per byte.  The slice by 8 algorithm is
    roughly 2X faster and enables packet processing at over 1GB/sec on a
    typical 2-3GHz system.
    
    Signed-off-by: Bob Pearson <rpearson@systemfabricworks.com>
    Cc: Roland Dreier <roland@kernel.org>
    Cc: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 79 */
+/* This file is auto generated, version 81 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#79 PREEMPT Thu Feb 23 14:40:20 CST 2012"
-#define LINUX_COMPILE_TIME "14:40:20"
+#define UTS_VERSION "#81 PREEMPT Thu Feb 23 16:40:45 CST 2012"
+#define LINUX_COMPILE_TIME "16:40:45"
 #define LINUX_COMPILE_BY "jason"
 #define LINUX_COMPILE_HOST "jason-kubuntu"
 #define LINUX_COMPILE_DOMAIN
